e6bcfc61c20106eb1f03def93d3b40ace0c51827
iqbaalleee <iqbalbjm489@gmail.com>
2024-10-23 16:38:45 +0800

    form login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('welcome', function () {
     return view('welcome');
 });
